1989ec7675de9beea266f68aaf4ba4ac12ff2638
曾鹏 <2031481080@qq.com>
2020-07-16 14:40:12 +0800

    'chat'

---
+++ nettyDemo/src/main/java/netty/chat/domain/Message.java
@@ -0,0 +1,40 @@
+package netty.chat.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 类说明: 消息实体
+ *
+ * @author zengpeng
+ */
+@Data
+public class Message implements Serializable {
+
+    /**
+     * 消息
+     */
+    private String message;
+
+    /**
+     * 发送时间
+     */
+    private Date sendTime;
+
+    /**
+     * 接收时间
+     */
+    private Date receivedTime;
+
+    /**
+     * 发送人ID
+     */
+    private Long senderId;
+
+    /**
+     * 接收人ID
+     */
+    private Long receiverId;
+}
